1d30b17bbac022457fc5d3e20d48cafe4befe850
Aditya Pandey <jerrypandey435@gmail.com>
2022-06-10 17:59:59 +0530

    init

--- atlas.py
+++ atlas.py
@@ -11,6 +11,9 @@ import pyttsx3
 import speech_recognition as sr    #pip install 
 import datetime
 import os
+import sys
+import 
+
 
 engine = pyttsx3.init('sapi5')
 
@@ -54,5 +57,36 @@ def TaskExecution():
             music_dir = ("C:\\Users\\jinal\\Music\\music.mp3")
             os.startfile(os.path.join(music_dir))
             # sys.exit()
-            
+
+        elif 'code red' or 'take a break' or 'take rest' in  query:
+             hour = int(datetime.datetime.now().hour)
+             if hour>=0 and hour<=12:
+                 speak("ok sir , have a nice day")
+                 sys.exit()
+
+             elif hour>=12 and hour<=18:
+                 speak("ok sir , have a nice day")
+                 sys.exit()   
+  
+             else:
+                 speak("ok sir, good night")
+                 speak("just let me know if you need any help")
+                 sys.exit()
+
+        elif 'who are you'  in query:
+             speak("hello i'm one off the most handsome A I in the world")
+             speak(", just kidding")
+             speak("hello i'm jaav,  an AI assistant") 
+             speak("i'm best example of artificial intelligence ")
+             speak("my main work is to assist my boss")
+             speak("i was made by some I T's students")
+
+        elif 'wikipedia' in  query:
+               speak("searching ', ' wikipedia' ")
+               query=  query.replace("wikipedia", "")
+               result = wi.summary(query,sentences = 2)
+               print(result)
+               speak("according to', ' wikipedia ")
+               speak(result)
+
 TaskExecution()
